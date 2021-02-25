3c6f93e3d39f925896652bfc072ef30538a53031
agathapresa <apresaga@ucsd.edu>
2021-02-25 11:23:37 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'wGAa3lErndwxGxvlYX6dNfXZPEEjDPgM';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+  	center: [32.87873652855066, -117.23578944340163],
+ 	 layers: L.mapquest.tileLayer('map'),
+ 	 zoom: 12,
+ 	 zoomControl: false
+	});
+	
+	// Add a marker to the map
+	L.marker([32.87873652855066, -117.23578944340163]).addTo(map);
 }
--- routes/rsvp.js
+++ routes/rsvp.js
@@ -8,3 +8,18 @@ var data = {
 exports.adminView = function(request, response){
   response.render('rsvp', data);
 };
+
+/*
+* handle POST request
+*/
+exports.addRSVP = function(request, response) {
+	var rsvpEmail = request.body.rsvpEmail;
+	// this will print to your terminal whenever the POST request is made
+	console.log(rsvpEmail);
+
+	data.rsvp.push(rsvpEmail); // add to current data
+
+	//let us send back some data so that
+	//broswer knows it worked!
+	response.send(rsvpEmail);
+};
